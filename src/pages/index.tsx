09ba21e119e2fb029c287a48820d7220ae5b11c5
binec <ferruscarea30@gmail.com>
2025-10-21 13:56:01 -0600

    imgs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,11 +37,11 @@ const galleryItems: GalleryItem[] = [
     client: 'Hako Sustainable Goods',
     duration: '6 semanas',
     year: '2023',
-    coverImage: '/img/projects/project-1/cover.jpg',
+    coverImage: 'public/assets/projects/project-1/cover.jpg',
     images: [
-      '/img/projects/project-1/image1.jpg',
-      '/img/projects/project-1/image2.jpg',
-      '/img/projects/project-1/image3.jpg',
+      '/assets/projects/project-1/image1.jpg',
+      '/assets/projects/project-1/image2.jpg',
+      '/assets/projects/project-1/image3.jpg',
     ]
   },
   { 
